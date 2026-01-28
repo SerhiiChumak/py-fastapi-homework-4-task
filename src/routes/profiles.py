6e2fd780f60e166cf038c6dd3f01a38a8d0504b7
Serhii Chumak <s4umak@gmail.com>
2026-01-28 14:10:41 +0200

    Modified profile in routes and schemas

--- src/routes/profiles.py
+++ src/routes/profiles.py
@@ -1,5 +1,43 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status, UploadFile, File, Form
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 
-router = APIRouter()
+from database import get_db
+from database.models.accounts import UserModel, UserProfileModel, GenderEnum
+from schemas.profiles import ProfileCreateSchema, ProfileResponseSchema
+from config.dependencies import get_accounts_email_notificator, get_jwt_auth_manager, get_s3_storage_client
 
-# Write your code here
+router = APIRouter(prefix="/profiles", tags=["Profiles"])
+
+
+@router.post("/", response_model=ProfileResponseSchema, status_code=status.HTTP_201_CREATED)
+async def create_profile(
+        first_name: str = Form(...),
+        last_name: str = Form(...),
+        gender: GenderEnum = Form(None),
+        date_of_birth: date = Form(None),
+        info: str = Form(None),
+        avatar: UploadFile = File(None),
+        db: AsyncSession = Depends(get_db),
+        token: str = Depends(get_accounts_email_notificator),
+        auth_manager=Depends(get_jwt_auth_manager),
+        s3_client=Depends(get_s3_storage_client)
+):
+    token_data = auth_manager.decode_access_token(token)
+    user_id = int(token_data.get("sub"))
+
+    query = select(UserModel).where(UserModel.id == user_id)
+    result = await db.execute(query)
+    user = result.scalar_one_or_none()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    if not user.is_active:
+        raise HTTPException(status_code=403, detail="Account not activated")
+
+    query_profile = select(UserProfileModel).where(UserProfileModel.user_id == user_id)
+    profile_exists = (await db.execute(query_profile)).scalar_one_or_none()
+    if profile_exists:
+        raise HTTPException(status_code=400, detail="Profile already exists")
+
+    return {"message"}
--- src/schemas/profiles.py
+++ src/schemas/profiles.py
@@ -10,4 +10,27 @@ from validation import (
     validate_birth_date
 )
 
-# Write your code here
+from pydantic import BaseModel, Field
+from datetime import date
+from typing import Optional
+from database.models.accounts import GenderEnum
+
+class ProfileCreateSchema(BaseModel):
+    first_name: str = Field(..., max_length=100, example="John")
+    last_name: str = Field(..., max_length=100, example="Doe")
+    gender: Optional[GenderEnum] = Field(None, example=GenderEnum.MAN)
+    date_of_birth: Optional[date] = Field(None, example="1990-01-01")
+    info: Optional[str] = Field(None, example="I love movies!")
+
+class ProfileResponseSchema(BaseModel):
+    id: int
+    first_name: Optional[str]
+    last_name: Optional[str]
+    avatar: Optional[str]
+    gender: Optional[GenderEnum]
+    date_of_birth: Optional[date]
+    info: Optional[str]
+    user_id: int
+
+    class Config:
+        from_attributes = True
